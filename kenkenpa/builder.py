bedc0c5d16700700b386722e856effc7fe1ef4b3
ONIHEI0910 <17534339+onihei0910@users.noreply.github.com>
2024-09-19 10:43:47 +0000

    Change the internal structure of StateGraphBuilder

--- kenkenpa/builder.py
+++ kenkenpa/builder.py
@@ -18,6 +18,7 @@ class StateGraphBuilder():
         ):
         self.graph_settings = graph_settings
         self.config_schema = config_schema
+
         if node_factorys:
             self.node_factorys = node_factorys
         else:
@@ -52,7 +53,6 @@ class StateGraphBuilder():
 
     def _gen_stategraph(self,stategraph_settings):
         stategraph_flow_parameter = stategraph_settings.get("flow_parameter")
-        print(stategraph_flow_parameter)
         state = stategraph_flow_parameter.get("state",[])
 
         self.custom_state = self.statebuilder.gen_state(state)
@@ -60,72 +60,91 @@ class StateGraphBuilder():
 
         for flow in stategraph_settings.get("flows",[]):
             graph_type = flow.get('graph_type')
-            flow_parameter = flow.get('flow_parameter',{})
-            factory_parameter = flow.get('factory_parameter',{})
 
             if graph_type == "stategraph":
-                node_name = flow_parameter['name']
-                substategraph = self._gen_stategraph(flow)
-                stategraph.add_node(node_name,substategraph.compile())
+                self._add_stategraph(stategraph,flow)
 
             elif graph_type == "node":
-                node_name = flow_parameter['name']
-                factory = flow_parameter['factory']
-
-                add_agent_function = self.node_factorys[factory]
-
-                node_func = add_agent_function(
-                    factory_parameter = factory_parameter,
-                    flow_parameter = flow_parameter,
-                    )
-
-                stategraph.add_node(node_name,node_func)
+                self._add_node(stategraph,flow)
 
             elif graph_type == "edge":
-                validate_keys(flow_parameter['start_key'],flow_parameter['end_key'])
-                
-                start_key_list = to_list_key(flow_parameter['start_key'])
-                end_key_list = to_list_key(flow_parameter['end_key'])
-                
-                for end_key in end_key_list:
-                    for start_key in start_key_list:
-                        stategraph.add_edge(
-                            start_key = start_key,
-                            end_key = end_key
-                        )
+                self._add_edge(stategraph,flow)
 
             elif graph_type == "static_conditional_edge":
-                start_key = flow_parameter['start_key']
-                conditions = flow_parameter['conditions']
-
-                edge_function = gen_static_conditional_edge(
-                    conditions = conditions,
-                    evaluate_functions = self.evaluete_functions
-                    )
-                
-                return_types = extract_literals(conditions)
-
-                stategraph.add_conditional_edges(
-                    source = start_key,
-                    path = edge_function,
-                    path_map = return_types
-                )
+                self._add_static_conditional_edge(stategraph,flow)
 
             elif graph_type == "static_conditional_entry_point":
-                conditions = flow_parameter['conditions']
+                self._add_static_conditional_entry_point(stategraph,flow)
 
-                edge_function = gen_static_conditional_edge(
-                    conditions = conditions,
-                    evaluate_functions = self.evaluete_functions
-                    )
-                
-                return_types = extract_literals(conditions)
-
-                stategraph.set_conditional_entry_point(
-                    path = edge_function,
-                    path_map = return_types
-                )
         return stategraph
+
+    def _add_stategraph(self,stategraph,flow):
+        flow_parameter = flow.get('flow_parameter',{})
+        node_name = flow_parameter['name']
+        substategraph = self._gen_stategraph(flow)
+        stategraph.add_node(node_name,substategraph.compile())
+
+    def _add_node(self,stategraph,flow):
+        flow_parameter = flow.get('flow_parameter',{})
+        factory_parameter = flow.get('factory_parameter',{})
+        node_name = flow_parameter['name']
+        factory = flow_parameter['factory']
+
+        add_agent_function = self.node_factorys[factory]
+
+        node_func = add_agent_function(
+            factory_parameter = factory_parameter,
+            flow_parameter = flow_parameter,
+            )
+
+        stategraph.add_node(node_name,node_func)
+    
+    def _add_edge(self,stategraph,flow):
+        flow_parameter = flow.get('flow_parameter',{})
+        validate_keys(flow_parameter['start_key'],flow_parameter['end_key'])
+        
+        start_key_list = to_list_key(flow_parameter['start_key'])
+        end_key_list = to_list_key(flow_parameter['end_key'])
+        
+        for end_key in end_key_list:
+            for start_key in start_key_list:
+                stategraph.add_edge(
+                    start_key = start_key,
+                    end_key = end_key
+                )
+
+    def _add_static_conditional_edge(self,stategraph,flow):
+        flow_parameter = flow.get('flow_parameter',{})
+        start_key = flow_parameter['start_key']
+        conditions = flow_parameter['conditions']
+
+        edge_function = gen_static_conditional_edge(
+            conditions = conditions,
+            evaluate_functions = self.evaluete_functions
+            )
+        
+        return_types = extract_literals(conditions)
+
+        stategraph.add_conditional_edges(
+            source = start_key,
+            path = edge_function,
+            path_map = return_types
+        )
+    def _add_static_conditional_entry_point(self,stategraph,flow):
+        flow_parameter = flow.get('flow_parameter',{})
+        conditions = flow_parameter['conditions']
+
+        edge_function = gen_static_conditional_edge(
+            conditions = conditions,
+            evaluate_functions = self.evaluete_functions
+            )
+        
+        return_types = extract_literals(conditions)
+
+        stategraph.set_conditional_entry_point(
+            path = edge_function,
+            path_map = return_types
+        )
 
 def extract_literals(conditions: List[Dict[str, Union[Dict, str]]]) -> str:
     results = []
