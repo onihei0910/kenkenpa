6902b5b7553e05fda8806e573008579105e1f4aa
ONIHEI0910 <17534339+onihei0910@users.noreply.github.com>
2024-09-21 09:22:36 +0000

    add static contitional edge model

---
+++ kenkenpa/models/static_conditional_edge.py
@@ -0,0 +1,22 @@
+from typing import List, Optional, Union, Dict, Any
+from pydantic import BaseModel, Field, root_validator,ConfigDict
+
+from kenkenpa.models.conditions import KConditionExpression
+from kenkenpa.models.conditions import KConditionDefault
+
+class KConditionalEdgeFlowParam_v1(BaseModel):
+    start_key:str
+    conditions:List[Union[KConditionExpression,KConditionDefault]]
+
+    model_config = ConfigDict(extra='forbid')
+
+KConditionalEdgeFlowParam = Union[KConditionalEdgeFlowParam_v1]
+
+class StaticConditionalEdge_v1(BaseModel):
+    graph_type:str
+    flow_parameter:KConditionalEdgeFlowParam
+    model_config = ConfigDict(extra='forbid')
+
+StaticConditionalEdge = Union[StaticConditionalEdge_v1]
+
+
---
+++ tests/unit_tests/models/static_conditional_edge_test.py
@@ -0,0 +1,49 @@
+import pytest
+from kenkenpa.models.static_conditional_edge import KConditionalEdgeFlowParam_v1
+from kenkenpa.models.static_conditional_edge import KConditionalEdgeFlowParam
+
+from kenkenpa.models.static_conditional_edge import StaticConditionalEdge_v1
+from kenkenpa.models.static_conditional_edge import StaticConditionalEdge
+
+def test_KConditionalEdgeFlowParam():
+    flow_parameter = {
+            "start_key":"agent",
+            "conditions":[
+                {
+                    "expression": {
+                        "eq": [
+                            {"type": "function", "name": "is_tool_message_function"},
+                            True
+                            ],
+                    },
+                    "result": "tools"
+                },
+                {"default": "END"} 
+            ]
+        }
+
+    KConditionalEdgeFlowParam_v1(**flow_parameter)
+    KConditionalEdgeFlowParam(**flow_parameter)
+
+def test_StaticConditionalEdge():
+    flow = {
+            "graph_type":"static_conditional_edge",
+            "flow_parameter":{
+                "start_key":"agent",
+                "conditions":[
+                    {
+                        "expression": {
+                            "eq": [
+                                {"type": "function", "name": "is_tool_message_function"},
+                                True
+                                ],
+                        },
+                        "result": "tools"
+                    },
+                    {"default": "END"} 
+                ]
+            },
+        }
+    
+    StaticConditionalEdge_v1(**flow)
+    StaticConditionalEdge(**flow)
